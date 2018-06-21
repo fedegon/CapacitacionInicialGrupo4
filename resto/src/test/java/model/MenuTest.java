53ff6e2c30540ee627c1249a82f2ab9b839257c7
Federico <fedegon@gmail.com>
2018-06-21 01:19:30 -0300

    FIX: change Junit Version to 5 (JAVA 8)

--- resto/src/test/java/model/MenuTest.java
+++ resto/src/test/java/model/MenuTest.java
@@ -1,56 +1,34 @@
-/**
- * 
- */
 package model;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-/**
- * @author fedex
- *
- */
-public class MenuTest {
+class MenuTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
+	@BeforeEach
+	void setUp() throws Exception {
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
+	@AfterEach
+	void tearDown() throws Exception {
 	}
 
-	/**
-	 * Test method for {@link java.lang.Object#toString()}.
-	 */
 	@Test
-	public void testToString() {
-		fail("Not yet implemented");
+	void testToString() {
+		fail("No implementado");
 	}
 
 }
