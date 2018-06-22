b65198b9b9a2ee7c3943fbafa21713fbecd22759
ALEJANDRO ABEL VALDEZ <ale.v_1988@hotmail.com>
2018-06-21 22:15:37 -0300

    The abstract class User is added.

---
+++ resto/src/main/java/model/User.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.awt.List;
+
+public abstract class User {
+	private String username;
+	private String name;
+	private String lastname;
+	private String password;
+	private String description;
+	private String photo;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	/*The collections of votes are missing, 
+	which will be added when the classes are constructed.*/
+	
+	public abstract String getRanking();
+	public abstract void addComment(String aComment);
+	/*public abstract List listRecommendations();*/
+	public abstract List listFriends();
+	public abstract User searchFriend(String aUsername);
+	public abstract void recommendRestaurant(User aUser);
+}
