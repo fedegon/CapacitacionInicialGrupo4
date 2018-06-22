da355fba2f584dc7fd1c403db366fb733dad75cc
ALEJANDRO ABEL VALDEZ <ale.v_1988@hotmail.com>
2018-06-21 22:42:39 -0300

    Class responsible for manipulating normal users is added

---
+++ resto/src/main/java/model/NormalUser.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.awt.List;
+
+public class NormalUser extends User {
+
+	@Override
+	public String getRanking() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addComment(String aComment) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List listFriends() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User searchFriend(String aUsername) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void recommendRestaurant(User aUser) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void voteRestaurant() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void voteMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void votePlate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
