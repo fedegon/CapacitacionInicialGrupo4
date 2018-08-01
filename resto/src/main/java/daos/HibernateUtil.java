f2f433021095ed7e01ce235ebc983237692afecc
ALEJANDRO ABEL VALDEZ <ale.v_1988@hotmail.com>
2018-08-01 16:48:41 -0300

    modifications in getSessionFactory and the class

--- resto/src/main/java/daos/HibernateUtil.java
+++ resto/src/main/java/daos/HibernateUtil.java
@@ -2,26 +2,38 @@ package daos;
 
 import java.io.Serializable;
 
+import javax.transaction.Transactional;
+
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public class HibernateUtil<T>{
+public class HibernateUtil<T> {
 
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
 	public HibernateUtil() {
 		// TODO Auto-generated constructor stub
-	
+
 	}
-	
-	public<T> Serializable create(T entity){
+	@Transactional
+	public <T> Serializable create(T entity) {
 		return sessionFactory.getCurrentSession().save(entity);
 	}
-	
+
 	public void update(T entity) {
 		sessionFactory.getCurrentSession().update(entity);
 	}
