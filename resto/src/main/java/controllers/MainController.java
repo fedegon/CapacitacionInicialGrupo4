92925529dd1f9bcf910754198e6557763684a0f7
ALEJANDRO ABEL VALDEZ <ale.v_1988@hotmail.com>
2018-08-01 16:52:49 -0300

    modifications to test.

--- resto/src/main/java/controllers/MainController.java
+++ resto/src/main/java/controllers/MainController.java
@@ -1,13 +1,21 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import daos.HibernateUtil;
 import daos.PlateDao;
 import daos.PlateDaoHibernate;
+import model.Menu;
 import model.Plate;
+import services.MenuService;
 import services.PlateService;
 
 @Controller
@@ -15,7 +23,9 @@ public class MainController {
 	@Autowired 
 	PlateService plateService;
 	@Autowired
-	PlateDao plateDao;
+	MenuService menuService;
+	@Autowired
+	HibernateUtil hibernateUtil;
 	/*@RequestMapping (value= {"index","/"},produces="text/plain;charset=UTF-8")
 	public ModelAndView index() {
 		return new ModelAndView("index");
@@ -24,8 +34,17 @@ public class MainController {
 	@RequestMapping(value= {"index","/","/resto/index"},produces="text/plain;charset=UTF-8")
 		public ModelAndView method() {
 		System.out.println("holaaaaaaaaaaa");
-		plateService.createPlate(new Plate("Pizzas", 120.00, "Bien rico"));
-		//plateDao.createPlate(new Plate("Tartas", 120.00, "Bien rico"));
+		Menu menu=new Menu();
+		List<Plate> listPlate=new ArrayList<>();
+		listPlate.add(new Plate("Pizzas", 120.00, "Bien rico",menu));
+		listPlate.add(new Plate("Bife", 60.00, "Bien criollo",menu));
+		listPlate.add(new Plate("Tartas", 80.00, "Bien rico",menu));
+		menu.setPlateList(listPlate);
+		hibernateUtil.create(menu);
+		//menuService.createMenu(menu);
+		
+		//plateService.createPlate(new Plate("Pizzas", 120.00, "Bien rico"));
+		
 		return new ModelAndView("index");
 	}
 
