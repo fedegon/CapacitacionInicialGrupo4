4ca20b5a55c9e1845db75277da277a7744065933
ALEJANDRO ABEL VALDEZ <ale.v_1988@hotmail.com>
2018-08-26 14:56:05 -0300

    Class DTO for restaurant is added

---
+++ resto/src/main/java/dto/RestaurantDTO.java
@@ -0,0 +1,57 @@
+package dto;
+
+import javax.persistence.Column;
+import javax.persistence.Transient;
+
+import model.Location;
+
+public class RestaurantDTO {
+	private long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String address;
+	
+	
+	public RestaurantDTO(long id, String name, String email, String phone, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
