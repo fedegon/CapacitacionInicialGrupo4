3016c17a3b8b7aadbcddb0c2d3a2e7398a08a3dc
ALEJANDRO ABEL VALDEZ <ale.v_1988@hotmail.com>
2018-08-31 23:02:06 -0300

    Method is added to transform DTO to the Object

--- resto/src/main/java/dto/RestaurantTransferAtDTO.java
+++ resto/src/main/java/dto/RestaurantTransferAtDTO.java
@@ -7,4 +7,14 @@ public class RestaurantTransferAtDTO {
 	public static RestaurantDTO transferAtDTO(Restaurant r) {
 		return new RestaurantDTO(r.getIdRestaurant(),r.getName(),r.getEmail(),r.getPhone(),r.getAddress());
 	}
+	public static Restaurant transferAtRestaurant(RestaurantDTO r) {
+		Restaurant resto= new Restaurant();
+		resto.setIdRestaurant(r.getId());
+		resto.setAddress(r.getAddress());
+		resto.setEmail(r.getEmail());
+		resto.setName(r.getName());
+		resto.setPhone(r.getPhone());
+		resto.setCategory("5 Estrellas");
+		return resto;
+	}
 }
